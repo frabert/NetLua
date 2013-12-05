cc5aaea2fa32878412c32a531c0f2deb044562a1
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2013-12-05 21:56:40 +0100

    Cleaned up syntax

--- NetLua/Libraries/MathLibrary.cs
+++ NetLua/Libraries/MathLibrary.cs
@@ -7,127 +7,128 @@ namespace NetLua
 {
     public static class MathLibrary
     {
-        static LuaArguments abs(LuaArguments args)
+        static LuaArguments math_abs(LuaArguments args)
         {
             return Lua.Return(Math.Abs(args[0]));
         }
 
-        static LuaArguments acos(LuaArguments args)
+        static LuaArguments math_acos(LuaArguments args)
         {
             return Lua.Return(Math.Acos(args[0]));
         }
 
-        static LuaArguments asin(LuaArguments args)
+        static LuaArguments math_asin(LuaArguments args)
         {
             return Lua.Return(Math.Asin(args[0]));
         }
 
-        static LuaArguments atan(LuaArguments args)
+        static LuaArguments math_atan(LuaArguments args)
         {
             return Lua.Return(Math.Atan(args[0]));
         }
 
-        static LuaArguments atan2(LuaArguments args)
+        static LuaArguments math_atan2(LuaArguments args)
         {
             return Lua.Return(Math.Atan2(args[0], args[1]));
         }
 
-        static LuaArguments ceil(LuaArguments args)
+        static LuaArguments math_ceil(LuaArguments args)
         {
             return Lua.Return(Math.Ceiling(args[0]));
         }
 
-        static LuaArguments cos(LuaArguments args)
+        static LuaArguments math_cos(LuaArguments args)
         {
             return Lua.Return(Math.Cos(args[0]));
         }
 
-        static LuaArguments cosh(LuaArguments args)
+        static LuaArguments math_cosh(LuaArguments args)
         {
             return Lua.Return(Math.Cosh(args[0]));
         }
 
-        static LuaArguments exp(LuaArguments args)
+        static LuaArguments math_exp(LuaArguments args)
         {
             return Lua.Return(Math.Exp(args[0]));
         }
 
-        static LuaArguments floor(LuaArguments args)
+        static LuaArguments math_floor(LuaArguments args)
         {
             return Lua.Return(Math.Floor(args[0]));
         }
 
-        static LuaArguments log(LuaArguments args)
+        static LuaArguments math_log(LuaArguments args)
         {
             return Lua.Return(Math.Log(args[0], args[1] | Math.E));
         }
 
-        static LuaArguments max(LuaArguments args)
+        static LuaArguments math_max(LuaArguments args)
         {
             return Lua.Return(Math.Max(args[0], args[1]));
         }
 
-        static LuaArguments min(LuaArguments args)
+        static LuaArguments math_min(LuaArguments args)
         {
             return Lua.Return(Math.Min(args[0], args[1]));
         }
 
-        static LuaArguments pow(LuaArguments args)
+        static LuaArguments math_pow(LuaArguments args)
         {
             return Lua.Return(Math.Pow(args[0], args[1]));
         }
 
-        static LuaArguments sin(LuaArguments args)
+        static LuaArguments math_sin(LuaArguments args)
         {
             return Lua.Return(Math.Sin(args[0]));
         }
 
-        static LuaArguments sinh(LuaArguments args)
+        static LuaArguments math_sinh(LuaArguments args)
         {
             return Lua.Return(Math.Sinh(args[0]));
         }
 
-        static LuaArguments sqrt(LuaArguments args)
+        static LuaArguments math_sqrt(LuaArguments args)
         {
             return Lua.Return(Math.Sqrt(args[0]));
         }
 
-        static LuaArguments tan(LuaArguments args)
+        static LuaArguments math_tan(LuaArguments args)
         {
             return Lua.Return(Math.Tan(args[0]));
         }
 
-        static LuaArguments tanh(LuaArguments args)
+        static LuaArguments math_tanh(LuaArguments args)
         {
             return Lua.Return(Math.Tanh(args[0]));
         }
 
         public static void AddMathLibrary(LuaContext Context)
         {
-            Dictionary<LuaObject, LuaObject> table = new Dictionary<LuaObject, LuaObject>();
-            table.Add("abs", (LuaFunction)abs);
-            table.Add("acos", (LuaFunction)acos);
-            table.Add("asin", (LuaFunction)asin);
-            table.Add("atan", (LuaFunction)atan);
-            table.Add("atan2", (LuaFunction)atan2);
-            table.Add("ceil", (LuaFunction)ceil);
-            table.Add("cos", (LuaFunction)cos);
-            table.Add("cosh", (LuaFunction)cosh);
-            table.Add("exp", (LuaFunction)exp);
-            table.Add("floor", (LuaFunction)floor);
-            table.Add("log", (LuaFunction)log);
-            table.Add("max", (LuaFunction)max);
-            table.Add("min", (LuaFunction)min);
-            table.Add("pow", (LuaFunction)pow);
-            table.Add("sin", (LuaFunction)sin);
-            table.Add("sinh", (LuaFunction)sinh);
-            table.Add("sqrt", (LuaFunction)sqrt);
-            table.Add("tan", (LuaFunction)tan);
-            table.Add("tanh", (LuaFunction)tanh);
+            dynamic math = LuaObject.NewTable();
+            math.abs = (LuaFunction)math_abs;
+            math.acos = (LuaFunction)math_acos;
+            math.asin = (LuaFunction)math_asin;
+            math.atan = (LuaFunction)math_atan;
+            math.atan2 = (LuaFunction)math_atan2;
+            math.ceil = (LuaFunction)math_ceil;
+            math.cos = (LuaFunction)math_cos;
+            math.cosh = (LuaFunction)math_cosh;
+            math.exp = (LuaFunction)math_exp;
+            math.floor = (LuaFunction)math_floor;
+            math.log = (LuaFunction)math_log;
+            math.max = (LuaFunction)math_max;
+            math.min = (LuaFunction)math_min;
+            math.pow = (LuaFunction)math_pow;
+            math.sin = (LuaFunction)math_sin;
+            math.sinh = (LuaFunction)math_sinh;
+            math.sqrt = (LuaFunction)math_sqrt;
+            math.tan = (LuaFunction)math_tan;
+            math.tanh = (LuaFunction)math_tanh;
 
-            table.Add("pi", Math.PI);
+            math.pi = Math.PI;
 
-            Context.SetGlobal("math", LuaObject.FromTable(table));
+
+            Context.Set("math", math);
         }
     }
 }
